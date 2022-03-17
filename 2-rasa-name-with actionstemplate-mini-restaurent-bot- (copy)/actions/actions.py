453f6f4cd1af028555471f25689d2f5b8d2cbf26
rabiyulfahimhasim786 <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2022-03-17 06:33:33 -0700

    Add files via upload

---
+++ 2-rasa-name-with actionstemplate-mini-restaurent-bot- (copy)/actions/actions.py
@@ -0,0 +1,63 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+from pipes import Template
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+#
+#
+class ActionHelloWorld(Action):
+#
+     def name(self) -> Text:
+         return "action_hello_world"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+         dispatcher.utter_message(text="Hello World!")
+
+         return []
+
+class ActionButtons(Action):
+#
+     def name(self) -> Text:
+         return "action_buttons"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+         dispatcher.utter_message(text="What would you looking for?", buttons=[
+            {"title": "Veg", "payload": "/vegeterian"},
+            {"title": "Non-veg", "payload": "/non_veg"},
+        ])
+
+         return []
